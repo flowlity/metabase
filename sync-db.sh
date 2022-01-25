a89bb9d410b6997963a329ca64fc773aa23ddd20
juncheng ZHOU <zhoujuncheng0116@gmail.com>
2022-01-25 10:13:01 +0100

    feat: update sync-db script

--- sync-db.sh
+++ sync-db.sh
@@ -6,6 +6,7 @@ docker-compose rm -fs postgres
   docker volume rm admin_manager_db-data
 } &> /dev/null
 
+TODAY=$(date +%Y_%m_%d)
 
 # Read remote database name & password from user
 echo "Remote database host:"
@@ -29,7 +30,9 @@ docker-compose up -d postgres
 echo -e "Dump database from remote DB"
 docker-compose run postgres \
 	pg_dump postgres://$REMOTE_DB_USER:$REMOTE_DB_PW@$REMOTE_DB_HOST:5432/$REMOTE_DB_NAME \
-	> $REMOTE_DB_NAME.sql
+	> ${REMOTE_DB_NAME}_${TODAY}.sql
 
 echo -e "Done ..."
 docker-compose down -v
+az storage file upload --account-name aksexternalglobal --share-name metabase --source ./${REMOTE_DB_NAME}_${TODAY}.sql
+rm ${REMOTE_DB_NAME}_${TODAY}.sql
